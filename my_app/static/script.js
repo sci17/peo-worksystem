f7bb77d1b9ddcceab34193f18d92c645764afc02
sci17 <recarzekaycee@gmail.com>
2026-03-04 22:20:14 +0700

    created registration

---
+++ my_app/static/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginCard = document.querySelector(".login-card");
+    if (loginCard) {
+        requestAnimationFrame(() => {
+            loginCard.classList.add("loaded");
+        });
+    }
+});
--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,26 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def user_dashboard(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('user_dashboard')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('user_dashboard')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
