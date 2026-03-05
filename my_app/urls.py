2c93c656d49dc1853112dd58b212b95202fd3df8
sci17 <recarzekaycee@gmail.com>
2026-03-05 15:48:28 +0700

    added construction dashboard and functions

--- my_app/urls.py
+++ my_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.user_dashboard, name='user_dashboard'),
     path('admin-division/', views.admin_division_dashboard, name='admin_division_dashboard'),
+    path('construction-division/', views.construction_division_dashboard, name='construction_division_dashboard'),
     path('maintenance/road-management/', views.road_management, name='road_management'),
     path('maintenance/contractor-management/', views.contractor_management, name='contractor_management'),
     path('maintenance/task-management/', views.task_management, name='task_management'),
