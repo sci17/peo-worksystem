84931b54adb8c2290b6fdf8674020d87dd2851de
peo-gov <provlengrsofficepalawan@mail.com>
2026-04-14 18:55:47 +0800

    fixed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,12 @@ set -e
 
 mkdir -p "${DJANGO_MEDIA_ROOT:-/shared/media}" /app/staticfiles /app/.cache/django
 
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+if [ "${RUN_MIGRATIONS:-false}" = "true" ]; then
+  python manage.py migrate --noinput
+fi
+
+if [ "${COLLECT_STATIC:-false}" = "true" ]; then
+  python manage.py collectstatic --noinput
+fi
 
 exec "$@"
